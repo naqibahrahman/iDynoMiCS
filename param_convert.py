e24a587765324915a7cd5851100fcf6d8c18a830
Robert Clegg <roughhawkbit@gmail.com>
2015-08-17 17:13:08 +0100

    extended param_convert.py so that it also converts the initial values

--- param_convert.py
+++ param_convert.py
@@ -7,12 +7,13 @@ import re
 
 # Where to find the SpeciesParam subclass
 file_dir = os.path.join('src',  'simulator', 'agent', 'zoo')
-file_name = ('ClostridiumParam')
+agent_file_name = 'Clostridium'
+param_file_name = 'ClostridiumParam'
 file_suffix = '.java'
 
 
 # Set this to true when you're happy the values should be changed on file
-overwrite_file = True
+overwrite_file = False
 
 
 # Units you want to use as defaults, and the relationship between default
@@ -21,11 +22,13 @@ mass_unit = 'fg'
 mass_scalar = 1e-15
 time_unit = 'h'
 time_scalar = 3600
-concn_unit = 'uM'
-concn_scalar = 1e-6
-vol_unit = 'mL'
+concn_unit = 'M'
+concn_scalar = 1e0
+vol_unit = 'L'
 vol_scalar = 1e-3
 
+
+old_concn = 'uM\n'
 
 #############################################################################
 
@@ -61,6 +64,7 @@ convert = {
 
 }
 
+print convert['nM']
 
 def convert_param(value, units):
     '''
@@ -70,7 +74,7 @@ def convert_param(value, units):
     units = units.replace('\n', '')
     float_val = float(value)
     for unit, [scalar, default] in convert.iteritems():
-        if scalar == 1.0:
+        if unit == default:
             continue
         power = ''
         try:
@@ -86,18 +90,22 @@ def convert_param(value, units):
             pass
     return ' '+str(float_val), units+'\n'
 
-file_path = os.path.join(file_dir, file_name+file_suffix)
 
-with open(file_path, 'r') as f:
+
+''' Parameter file '''
+
+param_file_path = os.path.join(file_dir, param_file_name+file_suffix)
+
+with open(param_file_path, 'r') as f:
     lines = f.readlines()
 
 
 for i in range(len(lines)):
     line = lines[i]
-    if file_name+'()' in line:
+    if param_file_name+'()' in line:
         break
     if 'public double' in line.lower():
-        [start, name, value, units] = re.split('ouble|;|=', line)
+        [start, name, value, units] = re.split('ouble|=|;', line)
         new_val, new_units = convert_param(value, units)
         if new_units != units:
             print('%s has changed\n  Old: %s %s\n  New: %s %s\n'
@@ -107,6 +115,39 @@ for i in range(len(lines)):
 
 
 if overwrite_file:
-    print 'Overwriting file...'
-    with open(file_path, 'w') as f:
+    print 'Overwriting parameter file...'
+    with open(param_file_path, 'w') as f:
+        f.write(''.join(lines))
+
+
+
+''' Agent file '''
+
+agent_file_path = os.path.join(file_dir, agent_file_name+file_suffix)
+
+with open(agent_file_path, 'r') as f:
+    lines = f.readlines()
+
+current_name = ''
+for i in range(len(lines)):
+    line = lines[i]
+    if '}' in line:
+        break
+    if '_proteinNames[' in line:
+        [start, value, units] = re.split('=|;', line)
+        current_name = value
+    if '_proteinLevels[' in line:
+        [start, value, units] = re.split('=|;', line)
+        units = old_concn
+        new_val, new_units = convert_param(value, units)
+        if new_units != units:
+            print('%s has changed\n  Old: %s %s\n  New: %s %s\n'
+                    %(current_name, value, old_concn.replace('\n',''),
+                      new_val, new_units.replace('\n','')))
+            lines[i] = line.replace(units, new_units).replace(value, new_val)
+
+
+if overwrite_file:
+    print 'Overwriting agent file...'
+    with open(agent_file_path, 'w') as f:
         f.write(''.join(lines))
